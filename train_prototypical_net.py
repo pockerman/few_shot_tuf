0be5123dbcef97abb01602d384edd8f6a1e4f733
Alex <a.giavaras@gmail.com>
2021-01-27 09:49:57 +0000

    Add train prototypical network script

---
+++ train_prototypical_net.py
@@ -0,0 +1,13 @@
+from training_engine import TrainEngine
+from prototypical_net_tuf import PrototypicalNetTUF
+from utils import timefn
+from utils import INFO
+
+@timefn
+def train():
+    pass
+
+
+if __name__ == '__main__':
+    print("{0} Training prototypical ne")
+    train()
