960181aa5b7dc91e4161e63865afba4b2351be1f
Alex <a.giavaras@gmail.com>
2021-01-27 14:40:45 +0000

    #3 Update network API

--- prototypical_net_tuf.py
+++ prototypical_net_tuf.py
@@ -8,10 +8,23 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+def conv_block_(in_channels, out_channels):
+    bn = nn.BatchNorm2d(out_channels)
+    nn.init.uniform_(bn.weight) # for pytorch 1.2 or later
+    return nn.Sequential(nn.Conv2d(in_channels, out_channels, 3, padding=1),
+                         bn, nn.ReLU(), nn.MaxPool2d(2))
+
+
 class PrototypicalNetTUF(nn.Module):
     """
     Class defining Prototypical network
     """
+
+    @staticmethod
+    def build_network(options):
+
+        device = 'cuda:0' if torch.cuda.is_available() and options.cuda else 'cpu'
+        return PrototypicalNetTUF().to(device=device)
 
     def __init__(self):
         super(PrototypicalNetTUF, self).__init__()
