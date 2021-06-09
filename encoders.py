765bd82216a8053f82ec44230e846c94f37ee3fa
Alex <a.giavaras@gmail.com>
2021-06-09 15:47:19 +0100

    Add new encoders

--- encoders.py
+++ encoders.py
@@ -15,6 +15,15 @@ def linear_with_softmax(in_features: int, out_features: int, bias: bool=True) ->
                          nn.Softmax())
 
 
+def convolution_with_linear_softmax(in_channels: int, out_channels: int, kernel_size: int,
+                                    in_features: int, out_features: int,
+                                    bias: bool=True) -> nn.Sequential:
+    return nn.Sequential(nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size),
+                         nn.Linear(in_features=in_features,
+                                   out_features=out_features, bias=bias),
+                         nn.ReLU(), nn.Softmax())
+
+
 def conv_block_(in_channels: int, out_channels: int, padding: int) -> nn.Sequential:
 
     bn = nn.BatchNorm2d(out_channels)
